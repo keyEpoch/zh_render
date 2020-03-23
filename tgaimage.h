b99f26d94fde0fa214fbe4cd9f46dfa2c89ce833
zhanghuan <1801210747@pku.edu.cn>
2020-03-23 23:50:25 +0800

    override [] for color

--- tgaimage.h
+++ tgaimage.h
@@ -34,22 +34,30 @@ struct TGAColor {
 	TGAColor() : val(0), bytespp(1) {
 	}
 
-	TGAColor(unsigned char R, unsigned char G, unsigned char B, unsigned char A) : b(B), g(G), r(R), a(A), bytespp(4) {
+	TGAColor(unsigned char R, unsigned char G, unsigned char B, unsigned char A) 
+    : b(B), g(G), r(R), a(A), bytespp(4) {
 	}
 
 	TGAColor(int v, int bpp) : val(v), bytespp(bpp) {
 	}
 
-	TGAColor(const TGAColor &c) : val(c.val), bytespp(c.bytespp) {
+	TGAColor(const TGAColor &c) 
+    : val(c.val), bytespp(c.bytespp) {
 	}
 
-	TGAColor(const unsigned char *p, int bpp) : val(0), bytespp(bpp) {
+	TGAColor(const unsigned char *p, int bpp) 
+    : val(0), bytespp(bpp) {
 		for (int i=0; i<bpp; i++) {
 			raw[i] = p[i];
 		}
 	}
 
-	TGAColor & operator =(const TGAColor &c) {
+    char operator[](int i) {
+        assert(i < 4);
+        return raw[i];
+    }
+
+	TGAColor& operator =(const TGAColor &c) {
 		if (this != &c) {
 			bytespp = c.bytespp;
 			val = c.val;
