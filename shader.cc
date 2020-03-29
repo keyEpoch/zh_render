3e8e7b9d5062d48fe65cb73b8ccb60bef8da4d3d
zhanghuan <1801210747@pku.edu.cn>
2020-03-29 18:19:19 +0800

    move implements to from .h to .cc

--- shader.cc
+++ shader.cc
@@ -23,7 +23,57 @@ bool OnlyTexShader::fragment(Vec3f bary, TGAColor& c, Model* model) {
     return false;
 }
 
+Vec4f CommonShader::vertex(int iface, int nthvert, Model* model) {
+        varying_uv.set_col(nthvert, model->uv(iface, nthvert));
+        
+        Vec4f nm_embeding = embed<4, 3, float>(model->normal(iface, nthvert), 0.f);
+        Vec3f transposed_norm = proj<3, 4, float>((Projection * ModelView).invert_transpose() * nm_embeding);
 
+        varying_norm.set_col(nthvert, transposed_norm);
+
+        // homogeneous coordinates
+        Vec4f homogeneous_vertex = Projection * ModelView * embed<4, 3, float>(model->vert(iface, nthvert), 1.f);
+        varying_triangle.set_col(nthvert, homogeneous_vertex);
+
+        back_homo_triangle.set_col(nthvert, proj<3, 4, float>(homogeneous_vertex / homogeneous_vertex[3]));
+
+        return homogeneous_vertex;
+    }
+
+bool CommonShader::fragment(Vec3f bary, TGAColor& c, Model* model) {
+    Vec3f bn = (varying_norm * bary).normalize();
+    Vec2f uv = varying_uv * bary;
+
+    mat<3, 3, float> A;
+    A[0] = back_homo_triangle.col(1) - back_homo_triangle.col(0);
+    A[1] = back_homo_triangle.col(2) - back_homo_triangle.col(0);
+    A[2] = bn;
+
+    mat<3, 3, float> AI = A.invert();
+
+    Vec3f i = AI * Vec3f(varying_uv[0][1] - varying_uv[0][0], varying_uv[0][2] - varying_uv[0][0], 0);
+    Vec3f j = AI * Vec3f(varying_uv[1][1] - varying_uv[1][0], varying_uv[1][2] - varying_uv[1][0], 0);
+
+    mat<3,3,float> B;
+    B.set_col(0, i.normalize());
+    B.set_col(1, j.normalize());
+    B.set_col(2, bn);
+
+    Vec3f n = (B * model->normal(uv)).normalize();
+
+    float diff = std::max(0.f, n * light_dir);
+    c = model->diffuse(uv) * diff;
+
+    return false;
+
+}
+
+
+void triangle(mat<4, 3, float>& pts, BaseShader& shader, TGAImage& image, float* zbuffer, Model* model) {
+
+}
+
+/*
 // this method to fill color in triangle can run parallel, so can us gpu for multi-threads
 void triangle(Vec3f* pts, BaseShader& shader, TGAImage& image, float* zbuffer, Model* model) {
     Vec2f bboxmin( std::numeric_limits<float>::max(),  std::numeric_limits<float>::max());
@@ -76,6 +126,7 @@ void triangle(Vec3f* pts, BaseShader& shader, TGAImage& image, float* zbuffer, M
         }
     }
 }
+*/
 
 Vec3f bary_centric(Vec2f A, Vec2f B, Vec2f C, Vec3f P) {
     Vec3f s[2];
@@ -113,7 +164,7 @@ void lookat(Vec3f eye, Vec3f center, Vec3f up) {
 }
 
 // after lookat(), the camera is always on z-axis
-void projection(float coeff = 0.f) {   // coeff = -1/c
+void projection(float coeff) {   // coeff = -1/c
     Projection = Matrix::identity();
     Projection[3][2] = coeff;
 }   
--- shader.h
+++ shader.h
@@ -55,52 +55,12 @@ public:
     mat<3, 3, float> varying_norm;
     mat<3, 3, float> back_homo_triangle;
 
-    virtual Vec4f vertex(int iface, int nthvert, Model* model) {
-        varying_uv.set_col(nthvert, model->uv(iface, nthvert));
-        
-        Vec4f nm_embeding = embed<4, 3, float>(model->normal(iface, nthvert), 0.f);
-        Vec3f transposed_norm = proj<3, 4, float>((Projection * ModelView).invert_transpose() * nm_embeding);
+    virtual Vec4f vertex(int iface, int nthvert, Model* model);
 
-        varying_norm.set_col(nthvert, transposed_norm);
-
-        // homogeneous coordinates
-        Vec4f homogeneous_vertex = Projection * ModelView * embed<4, 3, float>(model->vert(iface, nthvert), 1.f);
-        varying_triangle.set_col(nthvert, homogeneous_vertex);
-
-        back_homo_triangle.set_col(nthvert, proj<3, 4, float>(homogeneous_vertex / homogeneous_vertex[3]));
-
-        return homogeneous_vertex;
-    }
-
-    bool fragment(Vec3f bary, TGAColor& c, Model* model) {
-        Vec3f bn = (varying_norm * bary).normalize();
-        Vec2f uv = varying_uv * bary;
-
-        mat<3, 3, float> A;
-        A[0] = back_homo_triangle.col(1) - back_homo_triangle.col(0);
-        A[1] = back_homo_triangle.col(2) - back_homo_triangle.col(0);
-        A[2] = bn;
-
-        mat<3, 3, float> AI = A.invert();
-
-        Vec3f i = AI * Vec3f(varying_uv[0][1] - varying_uv[0][0], varying_uv[0][2] - varying_uv[0][0], 0);
-        Vec3f j = AI * Vec3f(varying_uv[1][1] - varying_uv[1][0], varying_uv[1][2] - varying_uv[1][0], 0);
-
-        mat<3,3,float> B;
-        B.set_col(0, i.normalize());
-        B.set_col(1, j.normalize());
-        B.set_col(2, bn);
-
-        Vec3f n = (B * model->normal(uv)).normalize();
-
-        float diff = std::max(0.f, n * light_dir);
-        c = model->diffuse(uv) * diff;
-
-        return false;
-
-    }
+    virtual bool fragment(Vec3f bary, TGAColor& c, Model* model);
 };
 
-void triangle(Vec3f* pts, BaseShader& shader, TGAImage& image, float* zbuffer, Model* model);
+// void triangle(Vec3f* pts, BaseShader& shader, TGAImage& image, float* zbuffer, Model* model);
+void triangle(mat<4, 3, float>& pts, BaseShader& shader, TGAImage& image, float* zbuffer, Model* model);
 
 Vec3f bary_centric(Vec2f A, Vec2f B, Vec2f C, Vec3f P);
